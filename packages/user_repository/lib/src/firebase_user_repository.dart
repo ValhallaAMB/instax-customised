670007dbaeba86f4980a75aa8cae428b4a196a97
ValhallaAMB <am.g.bashir@hotmail.com>
2025-08-03 15:37:45 +0300

    Updated the code inside packages folder

--- packages/user_repository/lib/src/firebase_user_repository.dart
+++ packages/user_repository/lib/src/firebase_user_repository.dart
@@ -1,62 +1,69 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:user_repository/src/models/my_user.dart';
 import 'entities/entities.dart';
 import 'user_repo.dart';
 
 class FirebaseUserRepository implements UserRepository {
-	FirebaseUserRepository({
-		FirebaseAuth? firebaseAuth,
-	})  : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+  FirebaseUserRepository({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
 
-	final FirebaseAuth _firebaseAuth;
-	final usersCollection = FirebaseFirestore.instance.collection('users');
+  /*
+  // Alternatively, you can use the default FirebaseAuth instance. However, you cannot use Final 
+  FirebaseUserRepository({FirebaseAuth? firebaseAuth}) {
+     _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+  } 
+  */
 
-	/// Stream of [MyUser] which will emit the current user when
-	/// the authentication state changes.
-	///
-	/// Emits [MyUser.empty] if the user is not authenticated.
-	@override
-	Stream<User?> get user {
-		return _firebaseAuth.authStateChanges().map((firebaseUser) {
-			final user = firebaseUser;
-			return user;
-		});
-	}
+  final FirebaseAuth _firebaseAuth;
+  final usersCollection = FirebaseFirestore.instance.collection('users');
 
-	@override
+  /// Stream of [MyUser] which will emit the current user when
+  /// the authentication state changes.
+  ///
+  /// Emits [MyUser.empty] if the user is not authenticated.
+  @override
+  Stream<User?> get user {
+    return _firebaseAuth.authStateChanges();
+
+    // Redundant code
+    /*
+    return _firebaseAuth.authStateChanges().map((firebaseUser) {
+      final user = firebaseUser;
+      return user;
+    });
+    */
+  }
+
+  @override
   Future<MyUser> signUp(MyUser myUser, String password) async {
     try {
       UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
-				email: myUser.email,
-				password: password
-			);
+          email: myUser.email, password: password);
 
-			myUser = myUser.copyWith(
-				id: user.user!.uid
-			);
+      myUser = myUser.copyWith(id: user.user!.uid);
 
-			return myUser;
+      // If the sign-up is successful, create a MyUser instance.
+      return myUser;
     } catch (e) {
       log(e.toString());
-			rethrow;
+      rethrow;
     }
   }
 
-	@override
+  @override
   Future<void> signIn(String email, String password) async {
     try {
       await _firebaseAuth.signInWithEmailAndPassword(
-				email: email,
-				password: password
-			);
+          email: email, password: password);
     } catch (e) {
       log(e.toString());
-			rethrow;
+      rethrow;
     }
   }
 
@@ -66,7 +73,7 @@ class FirebaseUserRepository implements UserRepository {
       await _firebaseAuth.signOut();
     } catch (e) {
       log(e.toString());
-			rethrow;
+      rethrow;
     }
   }
 
@@ -76,53 +83,46 @@ class FirebaseUserRepository implements UserRepository {
       await _firebaseAuth.sendPasswordResetEmail(email: email);
     } catch (e) {
       log(e.toString());
-			rethrow;
-    }
-  }
-
-	@override
-	Future<void> setUserData(MyUser user) async {
-		try {
-			await usersCollection.doc(user.id).set(user.toEntity().toDocument());
-		} catch(e) {
-			log(e.toString());
-			rethrow;
-		}
-	}
-
-	@override
-  Future<MyUser> getMyUser(String myUserId) async {
-    try {
-      return usersCollection.doc(myUserId).get().then((value) =>
-				MyUser.fromEntity(MyUserEntity.fromDocument(value.data()!))
-			);
-    } catch (e) {
-			log(e.toString());
       rethrow;
     }
   }
 
-	@override
-	 Future<String> uploadPicture(String file, String userId) async {
-		try {
-		  File imageFile = File(file);
-			Reference firebaseStoreRef = FirebaseStorage
-				.instance
-				.ref()
-				.child('$userId/PP/${userId}_lead');
-			await firebaseStoreRef.putFile(
-        imageFile,
-      );
-			String url = await firebaseStoreRef.getDownloadURL();
-			await usersCollection
-				.doc(userId)
-				.update({'picture': url});
-			return url;
-		} catch (e) {
-		  log(e.toString());
-			rethrow;
-		}
-	}
+  @override
+  Future<void> setUserData(MyUser user) async {
+    try {
+      await usersCollection.doc(user.id).set(user.toEntity().toDocument());
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
 
+  @override
+  Future<MyUser> getMyUser(String myUserId) async {
+    try {
+      return usersCollection.doc(myUserId).get().then((value) =>
+          MyUser.fromEntity(MyUserEntity.fromDocument(value.data()!)));
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
 
+  @override
+  Future<String> uploadPicture(String file, String userId) async {
+    try {
+      File imageFile = File(file);
+      List<int> imageBytes = imageFile.readAsBytesSync();
+      String base64Image = base64Encode(imageBytes);
+
+      await usersCollection.doc(userId).update({
+        'profilePictureUrl': base64Image,
+      });
+
+      return base64Image; // Return the base64 string of the image
+    } catch (e) {
+      log(e.toString());
+      rethrow; // Rethrow the exception to be handled by the caller
+    }
+  }
 }
